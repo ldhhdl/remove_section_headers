8c58155cbd903c9da2c961306706ecd3d21fd073
a <a>
2023-10-22 14:31:57 +0100

    starting file overwrite code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include <cstdlib>
 #include <elf.h>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <string>
+
+namespace fs = std::filesystem;
+
+std::string read_file(const std::string &filename) {
+  std::ifstream input_file{filename, std::ifstream::in | std::ifstream::binary};
+  if (!input_file.is_open() || !input_file.good()) {
+    std::cerr << "Error opening " << filename << std::endl;
+    return std::string{};
+  }
+  std::string file_data{std::istreambuf_iterator<char>(input_file),
+                        std::istreambuf_iterator<char>()};
+  input_file.close();
+  return file_data;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << " FILENAME" << std::endl;
     return EXIT_FAILURE;
   }
+
+  const std::string filename{argv[1]};
+  if (!fs::is_regular_file(filename)) {
+    std::cerr << filename << " does not exist" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto file_data = read_file(filename);
+  if (file_data.length() == 0) {
+    std::cerr << "Error reading " << filename << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // TODO: Overwrite section headers and shstrtab
+
   return 0;
 }
